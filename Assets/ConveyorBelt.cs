a907087f0f742765bb3634bc453d2295d1cb4cc7
MAT <101760335+mattyb91@users.noreply.github.com>
2024-02-22 15:30:42 +0000

    Conveyor Belt functionality
    Script that causes objects to move across the surface of another object (Conveyor Belt). Drag and drop functionality - Object returns to the location it was picked up at if its dropped in an invalid location. Drag and drop script to be added to Trash objects to allow them to be picked up by the player. Conveyor belt script to be added to the conveyor belt object. Trash items must have "MoveableObject" tag applied to them for full functionality.  - MAT

---
+++ Assets/ConveyorBelt.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{ 
+    public float speed = 1.0f;
+    public Vector3 direction = Vector3.right;
+
+    void Update()
+    {
+        // Move objects on top of the conveyor belt in the specified direction and speed
+        Collider[] colliders = Physics.OverlapBox(transform.position, transform.localScale / 2f);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("MoveableObject"))
+            {
+                Debug.Log("MoveableObject detected: " + collider.name);
+                collider.transform.Translate(direction * speed * Time.deltaTime);
+            }
+        }
+    }
+}
