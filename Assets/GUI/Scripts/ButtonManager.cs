8ebf245e329a9f0182375841af5135fa668533e2
goodguygeorge1 <gazvalorant@hotmail.com>
2024-03-12 17:17:05 +0000

    Added more stuff to GUI
    Changed background and font
    Added Music Slider
    Added Tutorial button - no functionality
    Exit and Play works
    Added a High Score

---
+++ Assets/GUI/Scripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void GoToScene () 
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application closed");
+    }
+
+}
