ab728d28638b2a2ecb8cc164e43acbcd425d38e4
Luke Walpole <71714679+lukewalpole@users.noreply.github.com>
2024-03-15 21:20:02 +0000

    Initial audio addition
    audio added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Audio Clip")]
+    public AudioClip mainGame;
+    public AudioClip conveyorBelt;
+    public AudioClip binOpen;
+    public AudioClip binClose;
+    public AudioClip itemRight;
+    public AudioClip itemWrong;
+    public AudioClip levelComplete;
+    public AudioClip levelFail;
+
+    private void Start()
+    {
+        musicSource.clip = mainGame;
+        musicSource.Play();
+        SFXSource.clip = conveyorBelt;
+        SFXSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
