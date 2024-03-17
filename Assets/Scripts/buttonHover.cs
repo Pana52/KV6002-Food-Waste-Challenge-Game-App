47bad00e000e36fb3354ba9390d54da57688a2e3
Luke Walpole <71714679+lukewalpole@users.noreply.github.com>
2024-03-17 19:10:05 +0000

    Button Hover sound added
    ButtonHover script added to play a sound effect whenever a user hovers their mouse over a ui button.

---
+++ Assets/Scripts/buttonHover.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverSound : MonoBehaviour, IPointerEnterHandler
+{
+    private AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (audioManager != null)
+        {
+            audioManager.PlaySFX(audioManager.buttonHover);
+        }
+    }
+}
