93f87f6ae296bf0e8d4c55e107ea9117227a3a6a
Panagiotis Theodorou <panayiotisth71@gmail.com>
2024-03-21 00:19:07 +0000

    Fixed the Dialogue System
    Added a placement on top of the screen for the dialogue system to take place
    
    coroutine improvements on when and how the dialogue is called and activated.

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,96 +1,100 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private GameObject textBG;
+
+    private Coroutine dialogueCoroutine = null; // Reference to the currently running showDialogue coroutine
+    private float textDuration = 3f;
 
     public void playDialogue(string type)
     {
-        if(type == "welcome")
+        if (type == "welcome")
         {
             welcomeDialogue();
         }
-        if (type == "success")
+        else if (type == "success")
         {
             successDiaglogue();
         }
-        if (type == "fail")
+        else if (type == "fail")
         {
             failDialogue();
         }
-        if (type == "gameOver")
+        else if (type == "gameOver")
         {
             GameOverDialogue();
         }
-        if (type == "level")
-        {
-            levelComplete();
-        }
-    }
-    void welcomeDialogue()
-    {
-        string[] message = {"Welcome!" };
-        StartCoroutine(showDialogue(message[Random.Range(0, message.Length)], 5f));
-    }
-    void successDiaglogue()
-    {
-        int combo = PlayerPrefs.GetInt("ComboValue");
-        if (combo == 5 || combo == 10 )
-        {
-            string[] message = { "Nice! That's " + combo + " correct bins in a row!", 
-                                  "Keep it up, that's " + combo + " correct bins in a row! "};
-            StartCoroutine(showDialogue(message[Random.Range(0, message.Length)], 5f));
-        }
-        else
-        {   
-            string[] message = { "Welldone!", "Good Job!" };
-            StartCoroutine(showDialogue(message[Random.Range(0, message.Length)], 5f));
-        }
-    }
-    void failDialogue()
-    {
-        int mistakes = PlayerPrefs.GetInt("IncorrectGuesses");
-        if (mistakes == 3 || mistakes == 5 )
-        {
-            string[] message = { "That's " + mistakes +  " mistakes in a row, please be careful. Improper recycling is harmful to the environment.",
-                                 "That's " + mistakes +  " mistakes in a row. Please refer to your guide if you get stuck." };
-            StartCoroutine(showDialogue(message[Random.Range(0, message.Length)], 5f));
-        }
-        else
-        {
-            string[] message = {"That's not the correct bin, try again.",
-                                "Hmmmm... That's not right." };
-            StartCoroutine(showDialogue(message[Random.Range(0, message.Length)], 5f));
-        }
-    }
-    void levelComplete()
-    {
-        if (PlayerPrefs.GetInt("CurrentLevel") == 4)
-        {
-            string[] message = { "Endless Mode" };
-            StartCoroutine(showDialogue(message[Random.Range(0, message.Length)], 5f));
-        }
-        else
-        {
-            string[] message = { "Level " + PlayerPrefs.GetInt("CurrentLevel").ToString() + "."};
-            StartCoroutine(showDialogue(message[Random.Range(0, message.Length)], 5f));
-        }
-    }
-    void GameOverDialogue()
-    {
-        string[] message = {"Alright, get outta here, you're messing up the place."};
-        StartCoroutine(showDialogue(message[Random.Range(0, message.Length)], 5f));
     }
 
-    IEnumerator showDialogue(string message, float duration)
+    private void welcomeDialogue()
     {
-        
-        dialogueText.text = message;
-        yield return new WaitForSeconds(duration);
-        dialogueText.text = "";
+        string[] message = { "Welcome!" };
+        ShowDialogue(message[Random.Range(0, message.Length)], textDuration);
     }
-    
+
+    private void successDiaglogue()
+    {
+        int combo = PlayerPrefs.GetInt("ComboValue");
+        string[] message;
+        if (combo == 5 || combo == 10)
+        {
+            message = new string[] { $"Nice! That's {combo} correct bins in a row!",
+                                     $"Keep it up, that's {combo} correct bins in a row! "};
+        }
+        else
+        {
+            message = new string[] { "Welldone!", "Good Job!" };
+        }
+        ShowDialogue(message[Random.Range(0, message.Length)], textDuration);
+    }
+
+    private void failDialogue()
+    {
+        int mistakes = PlayerPrefs.GetInt("IncorrectGuesses");
+        string[] message;
+        if (mistakes == 3 || mistakes == 5)
+        {
+            message = new string[] { $"That's {mistakes} mistakes in a row, please be careful. Improper recycling is harmful to the environment.",
+                                     $"That's {mistakes} mistakes in a row. Please refer to your guide if you get stuck." };
+        }
+        else
+        {
+            message = new string[] { "That's not the correct bin, try again.",
+                                     "Hmmmm... That's not right." };
+        }
+        ShowDialogue(message[Random.Range(0, message.Length)], textDuration);
+    }
+
+    private void GameOverDialogue()
+    {
+        string[] message = { "Alright, get outta here, you're messing up the place." };
+        ShowDialogue(message[Random.Range(0, message.Length)], textDuration);
+    }
+
+    // New method to manage dialogue display
+    private void ShowDialogue(string message, float duration)
+    {
+        if (dialogueCoroutine != null)
+        {
+            StopCoroutine(dialogueCoroutine);
+        }
+        dialogueCoroutine = StartCoroutine(ShowDialogueCoroutine(message, duration));
+    }
+
+    private IEnumerator ShowDialogueCoroutine(string message, float duration)
+    {
+        textBG.SetActive(true);
+        dialogueText.text = message;
+
+        yield return new WaitForSeconds(duration);
+
+        dialogueText.text = "";
+        textBG.SetActive(false);
+
+        dialogueCoroutine = null; // Reset the coroutine reference
+    }
 }
