d7826d72ab229585f23d933ae7d60b203f6d2227
Luke Walpole <71714679+lukewalpole@users.noreply.github.com>
2024-03-23 17:58:24 +0000

    OnOff Tidy up
    OnOff script.
    - tidied up code
    - added extra error handling
    - formatted comments

--- Assets/GUI/Scripts/OnOff.cs
+++ Assets/GUI/Scripts/OnOff.cs
@@ -1,34 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OnOff : MonoBehaviour
 {
-    // You can assign this through the Unity Inspector
     public GameObject targetGameObject;
     private AudioManager audioManager;
 
     private void Start()
     {
-        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        // Find AudioManager + Basic error handling
+        if (audioManager == null)
+        {
+            audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        }
     }
 
+    // Toggle active state and play sound of button click
     public void buttonClick()
     {
         if(audioManager != null)
         {
             audioManager.PlaySFX(audioManager.buttonPressed, 0.30f);
         }
-        else
-        {
-            Debug.LogWarning("AudioManager not found.Ensure there's a GameObject tagged with 'Audio' that has an AudioManager component.");
-        }
 
-
-        // Check if the targetGameObject is not null to avoid errors
         if (targetGameObject != null)
         {
-            // Toggle the active state
             targetGameObject.SetActive(!targetGameObject.activeSelf);
         }
     }
